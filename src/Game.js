4d1b1f3bb95b9f9f4402a7bb1ed86df913b5cc7b
fab1997 <93455997+fab1997@users.noreply.github.com>
2022-09-21 18:40:46 -0300

    repositorio listo para comenzar el trabajo. Fabian Valdez

---
+++ src/Game.js
@@ -0,0 +1,9 @@
+export default function Game()
+ { 
+    return(
+        <h1>Listo para comenzar el trabajo</h1>
+
+    );
+ }
+
+
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Game from './Game';
+
+
+
+
+const grupo1 = ReactDOM.createRoot(document.getElementById('grupo1'));
+grupo1.render(
+  <Game></Game>
+);
+
