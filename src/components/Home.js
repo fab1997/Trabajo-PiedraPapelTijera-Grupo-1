f4d6d26965acf78a5f634de8975f7cc711ba8f8b
RutJ <jaquelinacruz.95@gmail.com>
2022-09-23 20:55:39 -0300

    Creación del estilo de la página y el uso de sus botones. Cruz Ruth, Valdez Fabian

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,23 @@
 import "../styles/Home.css";
 import { Link } from "react-router-dom";
 import Inicio from "../img/principal.jpeg";
+import Button from "react-bootstrap/Button";
+
 export default function Home(){
     return(
         <div className="home">
             <h1>Piedra Papel o Tijera</h1>
-            <img src={Inicio} alt="Juego"/>
-
-            <Link to='/juego' className="btn-principal">Empezar a Jugar</Link>
-
-            <Link to='/desarrolladores' className="btn-principal">Conocenos</Link>
+            <img src={Inicio} alt="Juego"  className="rounded mx-auto d-block"/>
+            
+            <div className="d-grid gap-2 col-6 mx-auto " >
+                <Button variant="outline-info" size="lg" >
+                    <Link to='/juego' className="btn btn-principal">Empezar a Jugar</Link>
+                </Button>
+                <Button variant="outline-danger" size="lg">
+                    <Link to='/desarrolladores' className="btn btn-principal">Conocenos</Link>
+                </Button>
+               
+            </div>
         </div>
         
     )
