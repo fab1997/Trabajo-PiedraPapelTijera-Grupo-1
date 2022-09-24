d0f5f6f0ee3266181e30dc28683404301556dbe0
estradalautaro <estradalautaro3@gmail.com>
2022-09-24 07:10:18 -0300

    Actualizacion de juego
    Actualizacion de "Juego.js", Se crea las pestañas "Contador.js" y "Resultado.js" en "components"
    
    Co-Authored-By: BoccardoAnaCatalina <93290449+AnaCatalina@users.noreply.github.com>

---
+++ src/components/juego/Contador.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+//import './Contador.css';
+
+function Contador() {
+  const [numWinsJ1, setNumWinsJ1] = useState(0);
+  const [numWinsJ2, setNumWinsJ2] = useState(0);
+
+  const manejarWinsJ1 = () => {
+    setNumWinsJ1(numWinsJ1 + 1);
+  }
+
+  const manejarWinsJ2 = () => {
+    setNumWinsJ2(numWinsJ2 + 1);
+  }
+
+  return 
+//(
+//    <div className='contador'>
+//      {numWins}
+//    </div>
+ // );   
+}
+
+export default Contador;
---
+++ src/components/juego/Resultado.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Contador from "./components/juego/Contador";
+
+const Resultado = (props) => {
+  let resultadoFinal;
+
+  if (
+    props.jugador1.derrota === props.jugador2.eleccion &&
+    props.jugador1.eleccion
+  ) {
+    resultadoFinal = <h1>El Ganador es el Jugador 1</h1>;
+    manejarWinsJ1();
+  } else if (
+    props.jugador2.derrota === props.jugador1.eleccion &&
+    props.jugador1.eleccion
+  ) {
+    resultadoFinal = <h1>El Ganador es el Jugador 2</h1>;
+    manejarWinsJ2();
+  } else if (
+    props.jugador1.eleccion === props.jugador2.eleccion &&
+    props.jugador1.eleccion
+  ) {
+    resultadoFinal = <h1>Es un Empate</h1>;
+  }
+
+  return <h1>{resultadoFinal}</h1>;
+};
+
+export default Resultado;
