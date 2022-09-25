4f46a06d1bd23c7acf2610d42433abbedf319b6a
EliasDRobles <93445911+EliasDRobles@users.noreply.github.com>
2022-09-24 23:00:13 -0300

    creación del branch de desarrolladores y primera version de la página. Intregrantes de la rama Robles Elias David y Puca Alejandro

--- src/components/Desarrolladores.js
+++ src/components/Desarrolladores.js
@@ -1,5 +1,19 @@
-export default function Desarrolladores(){
-    return(
-        <h1>DevelopersPage</h1>
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import '../styles/desarrolladores.css';
+import HeaderDevs from './HeaderDevs';
+import DevsMain from './DevsMain';
+
+function Desarrolladores() {
+    return (
+        <>
+            <div className="container-fluid">
+                <HeaderDevs />
+                <DevsMain />                
+            </div>
+
+        </>
     )
 };
+
+export default Desarrolladores
---
+++ src/components/HeaderDevs.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class HeaderDevs extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Sobre nosotros</h1>
+                <h2>Los desarrolladores</h2>
+            </div>
+        )
+    }
+}
+
+export default HeaderDevs
