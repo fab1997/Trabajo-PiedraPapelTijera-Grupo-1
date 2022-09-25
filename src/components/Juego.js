07f5791aff64a8cf7ea35a02e7024d10eb28c153
AnaCatalina <93290449+AnaCatalina@users.noreply.github.com>
2022-09-25 05:30:46 -0300

    Cambios menores
    Algunos arreglos de espaciado y orden
    
    Co-Authored-By: estradalautaro <93290605+estradalautaro@users.noreply.github.com>

--- src/components/Juego.js
+++ src/components/Juego.js
@@ -7,79 +7,84 @@ import { Link } from "react-router-dom";
 import "../styles/Juego.css";
 
 export default function Juego() {
-    const [eleccionJugador1, setEleccionJugador1] = useState({});
-    const [eleccionJugador2, setEleccionJugador2] = useState({});
+  const [eleccionJugador1, setEleccionJugador1] = useState({});
+  const [eleccionJugador2, setEleccionJugador2] = useState({});
 
-    const opciones = [
-        {
-            eleccion: "Piedra",
-            derrota: "Tijera",
-        },
-        {
-            eleccion: "Papel",
-            derrota: "Piedra",
-        },
-        {
-            eleccion: "Tijera",
-            derrota: "Papel",
-        },
-    ];
+  const opciones = [
+    {
+      eleccion: "Piedra",
+      derrota: "Tijera",
+    },
+    {
+      eleccion: "Papel",
+      derrota: "Piedra",
+    },
+    {
+      eleccion: "Tijera",
+      derrota: "Papel",
+    },
+  ];
 
-    const eleccionRandomJ1 = () => {
-        const eleccion = opciones[Math.floor(Math.random() * opciones.length)];
-        setEleccionJugador1(eleccion);
-    };
+  const eleccionRandomJ1 = () => {
+    const eleccion = opciones[Math.floor(Math.random() * opciones.length)];
+    setEleccionJugador1(eleccion);
+  };
 
-    const eleccionRandomJ2 = () => {
-        const eleccion = opciones[Math.floor(Math.random() * opciones.length)];
-        setEleccionJugador2(eleccion);
-    };
+  const eleccionRandomJ2 = () => {
+    const eleccion = opciones[Math.floor(Math.random() * opciones.length)];
+    setEleccionJugador2(eleccion);
+  };
 
-    const hacerClic = () => {
-        eleccionRandomJ1();
-        eleccionRandomJ2();
-    }
+  const hacerClic = () => {
+    eleccionRandomJ1();
+    eleccionRandomJ2();
+  }
 
-    const volverHome = () => {
-        <Link to='/' className="btn-principal"></Link>
-    }
+  const volverHome = () => {
+    <Link to='/' className="btn-principal"></Link>
+  }
 
-    return (
-        <div className="juego">
-            <body>
+  return (
+    <div className="juego">
+      <body>
+        <main>
+          <section id="jugador1">
+            <div className="jugador1">Jugador 1</div>
+            <div className="eleccion">{eleccionJugador1.eleccion}</div>
+            <div clasName="imagen">ImagenActual</div>
+          </section>
+          <section id="jugador2">
+            <div className="jugador2">Jugador 2</div>
+            <div className="eleccion">{eleccionJugador2.eleccion}</div>
+            <div clasName="imagen">ImagenActual</div>
+          </section>
+          <section id="resultado">
+            <Resultado jugador1={eleccionJugador1} jugador2={eleccionJugador2} />
+          </section>
+          <div className='contadores'>
+            <section id="contadorJ1">
 
+            </section>
+            <section id="contadorJ2">
 
-                <h1>Esto es el juego</h1>
-                <main>
-                    <section>
-                        <div className="jugador1">Jugador 1</div>
-                        <div className="eleccion">{eleccionJugador1.eleccion}</div>
-                        <div clasName="imagen">ImagenActual</div>
-                    </section>
-                    <section>
-                        <div className="jugador2">Jugador 2</div>
-                        <div className="eleccion">{eleccionJugador2.eleccion}</div>
-                        <div clasName="imagen">ImagenActual</div>
-                    </section>
-                    <section>
-                        <Resultado jugador1={eleccionJugador1} jugador2={eleccionJugador2} />
-                    </section>
-
-
-
-                    <div className='contenedor-principal'>
-                        <a href="#"><Boton
-                            texto='Jugar'
-                            esBotonDeClic={true}
-                            manejarClic={hacerClic} /></a>
-
-                        <a href="http://localhost:3000" rel="noopener noreferrer"><Boton
-                            texto='Volver'
-                            esBotonDeClic={false}
-                            manejarClic={volverHome} /></a>
-                    </div>
-                </main>
-            </body>
-        </div>
-    );
+            </section>
+          </div>
+          <div className='botones'>
+            <section id="boton-jugar">
+              <a href="#"><Boton
+                texto='Jugar'
+                esBotonDeClic={true}
+                manejarClic={hacerClic} /></a>
+            </section>
+            <section id="boton-volver">
+              <a href="http://localhost:3000" rel="noopener noreferrer"><Boton
+                texto='Volver'
+                esBotonDeClic={false}
+                manejarClic={volverHome} /></a>
+            </section>
+          </div>
+        </main>
+      </body>
+    </div>
+  );
 };
--- src/components/juego/Imagen.js
+++ src/components/juego/Imagen.js
@@ -4,25 +4,25 @@ import Piedra from "../../img/juego/Piedra.png";
 import Tijera from "../../img/juego/Tijera.png";
 import Papel from "../../img/juego/Papel.png";
 
-function ImagenActual (props) {
-    const [ImagenObjeto, setImagenObjeto] = useState({});
-    const Imagen = <img src={ImagenBase} alt="ImagenBase" />;
-    setImagenObjeto(Imagen);
+function ImagenActual(props) {
+  const [ImagenObjeto, setImagenObjeto] = useState({});
+  const Imagen = <img src={ImagenBase} alt="ImagenBase" />;
+  setImagenObjeto(Imagen);
 
-    if ((props.jugador1.eleccion = "Piedra") || (props.jugador2.eleccion = "Piedra")) {
+  if ((props.jugador1.eleccion = "Piedra") || (props.jugador2.eleccion = "Piedra")) {
 
-        setImagenObjeto(<img src={Piedra} alt="ImagenPiedra" />);
+    setImagenObjeto(<img src={Piedra} alt="ImagenPiedra" />);
 
-    } else if ((props.jugador1.eleccion = "Tijera") || (props.jugador2.eleccion = "Tijera")) {
+  } else if ((props.jugador1.eleccion = "Tijera") || (props.jugador2.eleccion = "Tijera")) {
 
-        setImagenObjeto(<img src={Tijera} alt="ImagenTijera" />);
+    setImagenObjeto(<img src={Tijera} alt="ImagenTijera" />);
 
-    } else if ((props.jugador1.eleccion = "Papel") || (props.jugador2.eleccion = "Papel")) {
-        
-        setImagenObjeto(<img src={Papel} alt="ImagenPapel" />);
-    }
+  } else if ((props.jugador1.eleccion = "Papel") || (props.jugador2.eleccion = "Papel")) {
 
-    return ImagenObjeto;
+    setImagenObjeto(<img src={Papel} alt="ImagenPapel" />);
+  }
+
+  return ImagenObjeto;
 };
 
 export default ImagenActual;
