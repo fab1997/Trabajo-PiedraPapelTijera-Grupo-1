3ade31bb8d1a27c1bac9ec124b6acd343e8286ba
estradalautaro <estradalautaro3@gmail.com>
2022-09-25 23:01:13 -0300

    Visualizacion del Juego
    Se visualiza correctamente los colores del juego

--- src/components/Juego.js
+++ src/components/Juego.js
@@ -2,14 +2,19 @@ import React, { useState } from "react";
 import Boton from "../components/juego/Boton";
 import Contador from "../components/juego/Contador";
 import Resultado from "../components/juego/Resultado";
-import Imagen from "../components/juego/Imagen";
 import { Link } from "react-router-dom";
 import "../styles/Juego.css";
+import ImagenBase from "../img/juego/ImagenBase.png";
+import Piedra from "../img/juego/Piedra.png";
+import Tijera from "../img/juego/Tijera.png";
+import Papel from "../img/juego/Papel.png";
 
 export default function Juego() {
   const [eleccionJugador1, setEleccionJugador1] = useState("ImagenBase");
   const [eleccionJugador2, setEleccionJugador2] = useState("ImagenBase");
 
+  const [imgJugador, setImgJugador] = useState(<img src={ImagenBase} alt="ImagenBase" />);
+  const [imgJugador2, setImgJugador2] = useState(<img src={ImagenBase} alt="ImagenBase" />);
   const opciones = [
     {
       eleccion: "Piedra",
@@ -28,41 +33,68 @@ export default function Juego() {
   const eleccionRandomJ1 = () => {
     const eleccion = opciones[Math.floor(Math.random() * opciones.length)];
     setEleccionJugador1(eleccion);
+    
+    if (eleccion.eleccion == "Piedra") {
+  
+      setImgJugador(<img src={Piedra} alt="ImagenPiedra" />);
+      
+    } else if (eleccion.eleccion == "Tijera" ) {
+      
+      setImgJugador(<img src={Tijera} alt="ImagenTijera" />);
+  
+    } else if (eleccion.eleccion == "Papel") {
+      
+      setImgJugador(<img src={Papel} alt="ImagenPapel" />);
+    }
+    
   };
 
   const eleccionRandomJ2 = () => {
     const eleccion = opciones[Math.floor(Math.random() * opciones.length)];
     setEleccionJugador2(eleccion);
+    if (eleccion.eleccion == "Piedra") {
+  
+      setImgJugador2(<img src={Piedra} alt="ImagenPiedra" />);
+      
+    } else if (eleccion.eleccion == "Tijera" ) {
+      
+      setImgJugador2(<img src={Tijera} alt="ImagenTijera" />);
+  
+    } else if (eleccion.eleccion == "Papel") {
+      
+      setImgJugador2(<img src={Papel} alt="ImagenPapel" />);
+    }
   };
 
   const hacerClic = () => {
+    
     eleccionRandomJ1();
     eleccionRandomJ2();
+    
   }
 
   const volverHome = () => {
     <Link to='/' className="btn-principal"></Link>
   }
 
+
   return (
     <div className="juego">
-      <body>
-        <main>
-          <h3>
-            <section id="jugador1">
-              <div className="jugador1">Jugador 1</div>
-              <div className="eleccion">{eleccionJugador1.eleccion}</div>
-              <div clasName="imagen">{Imagen.ImagenActual}</div>
-              <div>Puntaje Jugador 1: {Contador}</div>
-            </section>
+      <h3>
+          <section id="jugador1">
+            <div className="jugador1">Jugador 1</div>
+            <div className="eleccion">{eleccionJugador1.eleccion}</div>
+            <div className="imagen">{imgJugador}</div>
+            <div>Puntaje Jugador 1: {Contador}</div>
+          </section>
           </h3>
           <h4>
-            <section id="jugador2">
-              <div className="jugador2">Jugador 2</div>
-              <div className="eleccion">{eleccionJugador2.eleccion}</div>
-              <div clasName="imagen">{Imagen.ImagenActual}</div>
-              <div>Puntaje Jugador 2: {Contador}</div>
-            </section>
+          <section id="jugador2">
+            <div className="jugador2">Jugador 2</div>
+            <div className="eleccion">{eleccionJugador2.eleccion}</div>
+            <div className="imagen">{imgJugador2}</div>
+            <div>Puntaje Jugador 2: {Contador}</div>
+          </section>
           </h4>
           <section id="resultado">
             <Resultado jugador1={eleccionJugador1} jugador2={eleccionJugador2} />
@@ -81,8 +113,7 @@ export default function Juego() {
                 manejarClic={volverHome} /></a>
             </section>
           </div>
-        </main>
-      </body>
+       
     </div>
   );
 };
